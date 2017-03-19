bfe89580c56aff9ac81fa4b17250881db57bb371
EC2 Default User <ec2-user@ip-172-31-49-113.ec2.internal>
2017-03-19 06:15:16 +0000

    R1.4.5 Mysql Again

--- docker/scripts/server_init_dev.sh
+++ docker/scripts/server_init_dev.sh
@@ -1,13 +1,14 @@
 # refer to the installation guide for Amazon EFS
 #sudo apt-get install -y nfs-common
-sudo umount /efs
+#sudo umount /efs
 sudo mkdir -p /efs
 sudo mkdir -p /var/lib/mysql 
 sudo mkdir -p /tmp/html 
 sudo mkdir -p /var/www/html 
 # the following fs name will change based on your actual EFS instance
-#sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-59ae0510.efs.us-east-1.amazonaws.com:/ /efs
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-fab912b3.efs.us-east-1.amazonaws.com:/ efs
+#sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-fab912b3.efs.us-east-1.amazonaws.com:/ efs
+#The above entry really should be in /etc/fstab and server rebooted.. 
+# WE are commenting this out for Neals usage of including in /etc/fstab
 
 # Keep changes together
 sudo useradd www-data
@@ -22,8 +23,11 @@ sudo chown -R www-data:www-data /var/www
 #Kill all ids.. 
 $HOME/drupal_cloud_formation/docker/scripts/kill_all.sh
 docker-compose -f $HOME/drupal_cloud_formation/docker/docker-compose.yml  up -d  
-#cp -r /tmp/html /var/www
+# uncomment id you need to run solr/nagios is same machine.. 
+#docker-compose -f $HOME/drupal_cloud_formation/docker/docker-compose-nagios-solr.yml  up -d  
+
 # at this point of time, if you see nothins under /var/www/html, the and only then 
 # cp -r /install_dir/* /var/www/html && chown -R www-data:www-data /var/www
+# do the above command from within the drupal_container
 echo "Done" 
 #rm -rf /tmp/html
