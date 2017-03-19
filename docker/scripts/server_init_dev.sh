ebd8bb55bcda0b3079c49d736616eef39000df69
EC2 Default User <ec2-user@ip-172-31-49-113.ec2.internal>
2017-03-19 06:16:45 +0000

    R1.4.5 Mysql Again

--- docker/scripts/server_init_dev.sh
+++ docker/scripts/server_init_dev.sh
@@ -6,7 +6,8 @@ sudo mkdir -p /var/lib/mysql
 sudo mkdir -p /tmp/html 
 sudo mkdir -p /var/www/html 
 # the following fs name will change based on your actual EFS instance
-#sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-fab912b3.efs.us-east-1.amazonaws.com:/ efs
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-fab912b3.efs.us-east-1.amazonaws.com:/ efs
+sudo service docker restart # required  after a reboot or re mounting..
 #The above entry really should be in /etc/fstab and server rebooted.. 
 # WE are commenting this out for Neals usage of including in /etc/fstab
 
